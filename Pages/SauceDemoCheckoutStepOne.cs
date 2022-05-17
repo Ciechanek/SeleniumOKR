60fac7230bc4b3c9e297bc8a39ec5a9d6c8ec00b
Mateusz Ciechan <Mateusz_Ciechan@edwards.com>
2022-05-17 21:45:39 +0200

    df

---
+++ Pages/SauceDemoCheckoutStepOne.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace SeleniumAppCiechanMateuszOKR.Pages
+{
+    public class SauceDemoCheckoutStepOne : BasePage
+    {
+        #region Buttons
+        IWebElement _cancelButton => Driver.FindElement(By.Id("cancel"));
+        IWebElement _continueButton => Driver.FindElement(By.Id("continue"));
+        IWebElement _firstNameInput => Driver.FindElement(By.Id("first-name"));
+        IWebElement _lastNameInput => Driver.FindElement(By.Id("last-name"));
+        IWebElement _postalCodeInput => Driver.FindElement(By.Id("postal-code"));
+        #endregion
+
+        public SauceDemoCheckoutStepOne (IWebDriver driver) : base (driver)
+        {
+
+        }
+
+        public void ClickCancelButton()
+        {
+            _cancelButton.Click();
+        }
+
+        public void ClickContinueButton()
+        {
+            _continueButton.Click();
+        }
+
+        public void EnterFirstName(string firstName)
+        {
+            _firstNameInput.SendKeys(firstName);
+        }
+        public void EnterLastName(string lastName)
+        {
+            _firstNameInput.SendKeys(lastName);
+        }
+
+        public void EnterPostalCode(string postalCode)
+        {
+            _firstNameInput.SendKeys(postalCode);
+        }
+    }
+}
---
+++ Steps/EnterPersonalData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SeleniumAppCiechanMateuszOKR.Pages;
+
+namespace SeleniumAppCiechanMateuszOKR.Steps
+{
+    
+    public class EnterPersonalData : BaseSteps
+    {
+        SauceDemoCheckoutStepOne sauceDemoCheckoutStepOne => new SauceDemoCheckoutStepOne(Driver);
+        public EnterPersonalData(IWebDriver driver) : base(driver)
+        {
+
+        }
+
+        public void EnterValidPersonalDataToTheOrder(string firstName, string lastName, string postalCode)
+        {
+            sauceDemoCheckoutStepOne.EnterFirstName(firstName);
+            sauceDemoCheckoutStepOne.EnterLastName(lastName);  
+            sauceDemoCheckoutStepOne.EnterPostalCode(postalCode);
+            sauceDemoCheckoutStepOne.ClickContinueButton();
+        }
+
+        public void EnterPersonalDataToTheOrderWithoutFirstName(string lastName, string postalCode)
+        {
+            sauceDemoCheckoutStepOne.EnterFirstName("");
+            sauceDemoCheckoutStepOne.EnterLastName(lastName);
+            sauceDemoCheckoutStepOne.EnterPostalCode(postalCode);
+            sauceDemoCheckoutStepOne.ClickContinueButton();
+        }
+    }
+}
